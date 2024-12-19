bf37ae0f351078be759b446296c9cef7f054a66c
Wissald22 <wissaldahibi22@gmail.com>
2024-12-19 11:02:18 +0100

    test pour 2

--- src/test/java/com/pantxi/romannumbers/ArabicRomanNumeralsTest.java
+++ src/test/java/com/pantxi/romannumbers/ArabicRomanNumeralsTest.java
@@ -9,8 +9,8 @@ public class ArabicRomanNumeralsTest {
     @Test
     public void convertest() {
         assertEquals("II", ArabicRomanNumerals.convert1_2_3(2));
-        assertEquals("X", ArabicRomanNumerals.convert_nbrxdix(10));
+        /*assertEquals("X", ArabicRomanNumerals.convert_nbrxdix(10));
         assertEquals("IX", ArabicRomanNumerals.convert_unite(9));
-        assertEquals("XIX", ArabicRomanNumerals.convertnombre(19));
+        assertEquals("XIX", ArabicRomanNumerals.convertnombre(19)); */
     }
 }
