9b5dda2e7df0ff940faffed0505a9b5a82bd12c1
Wissald22 <wissaldahibi22@gmail.com>
2024-12-19 11:19:48 +0100

    test not passing 19

--- src/test/java/com/pantxi/romannumbers/ArabicRomanNumeralsTest.java
+++ src/test/java/com/pantxi/romannumbers/ArabicRomanNumeralsTest.java
@@ -11,6 +11,6 @@ public class ArabicRomanNumeralsTest {
        /* assertEquals("II", ArabicRomanNumerals.convert1_2_3(2));
        assertEquals("X", ArabicRomanNumerals.convert_nbrxdix(10));
         assertEquals("IX", ArabicRomanNumerals.convert_unite(9));*/
-        assertEquals("XIX", ArabicRomanNumerals.convertnombre(19));
+        assertEquals("XX", ArabicRomanNumerals.convertnombre(19));
     }
 }
